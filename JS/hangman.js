ffc30ac3402cc9d413d299a169fa571d22008653
Kyle Howard <howardka@hotmail.com>
2016-02-14 20:57:55 -0500

    basic layout done and character replacement

--- JS/hangman.js
+++ JS/hangman.js
@@ -4,10 +4,32 @@ window.onload= function(){
 	var placeholder = "";
 	var wordlength = word.length;
 
+	document.getElementById("submitGuess").addEventListener("click", checkGuess);
+
 	for(i=0;i<wordlength;i++){
-		placeholder +="_ "
+		placeholder +="_"
 	}
 
 	document.getElementById("wordspace").innerHTML=placeholder;
 
+	function checkGuess(){
+		var indices=[];
+		letter = document.getElementById("letterGuess").value;
+		for(i=0;i<wordlength;i++){
+			if(word[i]==letter){
+				indices.push(i);
+				temp = placeholder.split("");
+				temp[i]=letter;
+				placeholder=temp.join("");
+				console.log(placeholder);
+			}
+		}
+
+		document.getElementById("wordspace").innerHTML=placeholder;
+		console.log("done")
+
+	}
 }
+
+
+
